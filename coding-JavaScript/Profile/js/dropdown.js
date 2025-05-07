e5826d73b1af13a74e80bc7e524da2a7246d0d72
k-hokari <ks16436hk@gmail.com>
2025-05-07 17:55:45 +0900

    Profile_fix/dropdown-modal2

--- coding-JavaScript/Profile/js/dropdown.js
+++ coding-JavaScript/Profile/js/dropdown.js
@@ -1,19 +1,23 @@
 //ドロップダウンの設定を関数でまとめる
 function mediaQueriesWin(){
-	var width = $(window).width();
-	if(width <= 768) {//横幅が768px以下の場合 $(".has-child>a").off('click');	//has-childクラスがついたaタグのonイベントを複数登録を避ける為offにして一旦初期状態へ
-		$(".has-child>a").on('click', function() {//has-childクラスがついたaタグをクリックしたら
-			var parentElem =  $(this).parent();// aタグから見た親要素のliを取得し
-			$(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
-			$(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
-			return false;//リンクの無効化
-		});
-	}else{//横幅が768px以上の場合
-		$(".has-child>a").off('click');//has-childクラスがついたaタグのonイベントをoff(無効)にし
-		$(".has-child").removeClass('active');//activeクラスを削除
-		$('.has-child').children('ul').css("display","");//スライドトグルで動作したdisplayも無効化にする
-	}
+	$('.has-child').hover(
+		function() {
+			$(this).children('.dropdown-style').css({
+				display: 'flex',
+				visibility: 'visible',
+				opacity: 1
+			});
+		},
+		function() {
+			$(this).children('.dropdown-style').css({
+				display: 'none',
+				visibility: 'hidden',
+				opacity: 0
+			});
+		}
+	) 
 }
+
 
 // ページがリサイズされたら動かしたい場合の記述
 $(window).resize(function() {
