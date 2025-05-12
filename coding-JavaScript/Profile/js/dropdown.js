aabec18a097e49fe865d3d53d636912abf757ba7
k-hokari <ks16436hk@gmail.com>
2025-05-12 17:56:56 +0900

    Profile_fix/dropdown-modal3

--- coding-JavaScript/Profile/js/dropdown.js
+++ coding-JavaScript/Profile/js/dropdown.js
@@ -1,30 +1,22 @@
-//ドロップダウンの設定を関数でまとめる
 function mediaQueriesWin(){
-	$('.has-child').hover(
-		function() {
-			$(this).children('.dropdown-style').css({
-				display: 'flex',
-				visibility: 'visible',
-				opacity: 1
-			});
-		},
-		function() {
-			$(this).children('.dropdown-style').css({
-				display: 'none',
-				visibility: 'hidden',
-				opacity: 0
-			});
-		}
-	) 
+  if (window.innerWidth > 768) {
+    $('.has-child').off('mouseenter mouseleave').hover(
+      function() {
+        $(this).children('.dropdown-style').css({
+          display: 'flex',
+          visibility: 'visible',
+          opacity: 1
+        });
+      },
+      function() {
+        $(this).children('.dropdown-style').css({
+          display: 'none',
+          visibility: 'hidden',
+          opacity: 0
+        });
+      }
+    );
+  } else {
+    $('.has-child').off('mouseenter mouseleave'); // SPではhoverイベント無効
+  }
 }
-
-
-// ページがリサイズされたら動かしたい場合の記述
-$(window).resize(function() {
-	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
-});
-
-// ページが読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load',function(){
-	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
-});
